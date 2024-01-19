c5603131f0c78566e74b04e2cdb39338f7f7599e
Matheus Gadelha <gadelha@adobe.com>
2024-01-18 21:40:01 -0800

    First commit. All basic plugins should be here.

---
+++ init.lua
@@ -0,0 +1,28 @@
+---@diagnostic disable: undefined-global
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.cmd("set number")
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+if vim.fn.has("termguicolors") then
+  vim.opt.termguicolors = true
+end
+
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system {
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release
+    lazypath,
+  }
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup("plugins")
---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,11 @@
+return {
+  --"rockerBOO/boo-colorscheme-nvim",
+  --config = function()
+  --  vim.cmd.colorscheme "sunset_cloud"
+  --end
+  "aktersnurra/no-clown-fiesta.nvim",
+  config = function()
+    vim.cmd.colorscheme "no-clown-fiesta"
+  end
+}
+
