73bb361006f691e0855de87e4bbdb3d5b3c415ff
Matheus Gadelha <gadelha@adobe.com>
2024-01-18 22:00:45 -0800

    Fix in `completions.lua`
    Commented `require('lspconfig')['<YOUR_LSP_SERVER>']` in
    `completions.lua` that was yielding errors in init.

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -66,12 +66,12 @@ return {
         })
       })
 
-      -- Set up lspconfig.
-      local capabilities = require('cmp_nvim_lsp').default_capabilities()
-      -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
-      require('lspconfig')['<YOUR_LSP_SERVER>'].setup {
-        capabilities = capabilities
-      }
+     -- -- Set up lspconfig.
+     -- local capabilities = require('cmp_nvim_lsp').default_capabilities()
+     -- -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
+     -- require('lspconfig')['<YOUR_LSP_SERVER>'].setup {
+     --   capabilities = capabilities
+     -- }
     end
   }
 }
