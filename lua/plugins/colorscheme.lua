7083a10576b0c7ee8e867103ff49be35300d9373
Matheus Gadelha <gadelha@adobe.com>
2024-03-04 22:39:54 +0000

    Changed color scheme.

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,9 +3,13 @@ return {
   --config = function()
   --  vim.cmd.colorscheme "sunset_cloud"
   --end
-  "aktersnurra/no-clown-fiesta.nvim",
+  -- "aktersnurra/no-clown-fiesta.nvim",
+  -- config = function()
+  --   vim.cmd.colorscheme "no-clown-fiesta"
+  -- end
+  "bluz71/vim-moonfly-colors",
   config = function()
-    vim.cmd.colorscheme "no-clown-fiesta"
+    vim.cmd.colorscheme "moonfly"
   end
 }
 
